3091fb714eb579d112dbd5cd9b5e7244e85fcbce
MariusVinclair <marius.vinclair@gmail.com>
2024-04-02 12:44:42 +0200

    prix produit début
    prix produit début

---
+++ classes/prix.py
@@ -0,0 +1,7 @@
+class Prix:
+    def __init__(self, devise, montant):
+        self.devise = devise
+        self.montant = montant
+    
+    def _conversioneuros(self):
+        return self.montant
--- fonctions/calculindice.py
+++ fonctions/calculindice.py
@@ -1,26 +1,24 @@
-from article import Article
-from prix import Prix
+
+
+from ..classes.article import Article
 import numpy as np
 
 
 class Produit:
-    def __init__(self, nom: str, articles: dict):
+    def __init__(self, nom, articles):
         self._nom = nom
         self._articles = articles
-
+    
     def _calculindicesproduit(self):
         indices = dict()
         prix_prod = dict()
-        L = []
-        M = []
-
+        L=[]
         for key in self._articles.keys():
-            i = 0
-            prix_prod_moyen = 0
+            i=0
             for key2 in self._articles.keys():
                 if self._articles[key]._pays == self._articles[key2]._pays:
-                    prix_prod_moyen += self._articles[key]._prix._conversioneuros()
-                    i += 1
+                    prix_prod_moyen +=  self._articles[key].prix._conversioneuros()
+                    i+=1
             prix_prod_moyen /= i
             prix_prod[self._articles[key]._pays] = prix_prod_moyen
 
@@ -30,24 +28,11 @@ class Produit:
         vai = np.percentile(L, 25) - (np.percentile(L, 75)-np.percentile(L, 25))*1.5
         vas = np.percentile(L, 75) + (np.percentile(L, 75)-np.percentile(L, 25))*1.5
         
-        
         for keys in prix_prod.keys():
             if prix_prod[keys] < vai or prix_prod[keys] > vas:
                 prix_prod[keys] = None
         
-        for value in prix_prod.values():
-            M.append(value)
-        
-        prix_max = max(M)
-        prix_min = min(M)
-
-        
+        ind1 = (prix_prod - prix_min) / (prix_max - prix_min)
 
         for key in self._articles.keys():
-            ind1 = (prix_prod[self._articles[key]._pays] - prix_min) / (prix_max - prix_min)
             indices[self._articles[key]._pays] = ind1
-
-p1 = Produit('riz', {'001': Article('001', Prix('EUR',50.0), 'France'), '002': Article('002', Prix('EUR',20.0), 'France'),'003': Article('003', Prix('EUR',30.0), 'France')})
-
-print(p1._articles)
-print(p1._calculindicesproduit())
