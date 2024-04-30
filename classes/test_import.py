0866af187e7cf358987e188ee919626de2ab7acd
Sjats <149076885+Sjats@users.noreply.github.com>
2024-04-30 12:30:43 +0200

    test

--- classes/test_import.py
+++ classes/test_import.py
@@ -1,6 +1,21 @@
-import geojson
+import sys
+import os
 
-with open("classes/world_country_boundaries.geojson.json") as f:
-    gj = geojson.load(f)
-features = gj['features'][0]
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 
+# Import pour histogramme :
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+# Import pour la carte interactive
+import streamlit as st
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+
+chemin_geojson = 'classes/world_country_boundaries.geojson.json'
+gdf = gpd.read_file(chemin_geojson) # Chemin vers le fichier GeoJSON
+print(gdf)
