97fc428dbf89d1dfb66b5fd469132c537082eedf
Sjats <149076885+Sjats@users.noreply.github.com>
2024-04-30 12:07:02 +0200

    aa

--- classes/test_import.py
+++ classes/test_import.py
@@ -1,7 +1,6 @@
-import json
-from pprint import pprint
+import geojson
 
-with open('/Users/Sunifred/Documents/GitHub/ProjetTraitementdeDonnees2024/classes/world_country_boundaries.geojson.json') as json_data:
-    d = json.loads(json_data)
-    json_data.close()
-    pprint(d)
+with open("classes/world_country_boundaries.geojson.json") as f:
+    gj = geojson.load(f)
+features = gj['features'][0]
+
