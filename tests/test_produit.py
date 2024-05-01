7010927e63f1f8b77cc2f5a7e258b436981fec87
MariusVinclair <marius.vinclair@gmail.com>
2024-05-01 16:00:38 +0200

    test categorie

--- tests/test_produit.py
+++ tests/test_produit.py
@@ -4,33 +4,35 @@ from classes.prix import Prix
 import pytest
 import re
 
+
 @pytest.mark.parametrize(
     'produit', 'erreur', 'message_erreur',
     [
-        (Produit(['seche_cheveux'], {'seche_cheveux1': Article(id_article,
-                                                               Prix(30, 'EUR'),
-                                                               'France')}),
+        (Produit(['coffee_maker'], {'coffee_maker1':
+                                    Article('France/Coffee+maker/0',
+                                            Prix(30, 'EUR'),
+                                            'France')}),
          TypeError, "Le nom doit être une instance de str."),
 
-        (Produit('seche_cheveux', Article(id_article,
-                                            Prix(30, 'EUR'),
-                                            'France')),
+        (Produit('coffee_maker', Article('France/Coffee+maker/0',
+                                         Prix(30, 'EUR'),
+                                         'France')),
          TypeError, "Les articles doivent être une instance de dictionnaire."),
 
-        (Produit('seche_cheveux', { 1 : Article(id_article,
-                                                Prix(30, 'EUR'),
-                                                'France')}),
+        (Produit(['coffee_maker'], {0: Article('France/Coffee+maker/0',
+                                               Prix(30, 'EUR'),
+                                               'France')}),
          TypeError, (
              "Les clés du dictionnaire d'articles doivent être une "
              "instance de str."
          )),
-        
-        (Produit('seche_cheveux', { 'seche_cheveux1' : Prix(30, 'EUR')}),
+
+        (Produit(['coffee_maker'], {'coffee_maker1': Prix(30, 'EUR')}),
          TypeError, (
              "Les valeurs du dictionnaire d'articles doivent être une "
              "instance d'Article"
          )),
-        
+
      ]
 )
 def test_produit_init_echec(produit, erreur, message_erreur):
