dda01507b84c61ce702fe1eeb01f4aa1ed661fef
Sjats <149076885+Sjats@users.noreply.github.com>
2024-05-02 12:24:31 +0200

    nbb

--- interface/interface_sd.py
+++ interface/interface_sd.py
@@ -2,9 +2,19 @@ import keyboard
 import time
 import os
 import pickle
+from interface_admin import InterfaceAdmin
+
 etoiles = "***********************************************"
 "****************************"
 
 
-
 class InterfaceSd:
+    def __init__(self, admin=False):
+        self._admin = admin
+        print(etoiles)
+        print("Bienvenu sur l'iterface d'affichage")
+        print(etoiles)
+        self.main_menu()
+
+    def admint_plus(self):
+        print("[a] Retour menu administrateur")
