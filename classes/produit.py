fbef89b44f12f132c8bba0d57af62d361009ea4b
Sjats <149076885+Sjats@users.noreply.github.com>
2024-05-03 10:47:08 +0200

    net

--- classes/produit.py
+++ classes/produit.py
@@ -1,8 +1,4 @@
-import sys
 import os
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
 import pickle
 from classes.article import Article
 import numpy as np
