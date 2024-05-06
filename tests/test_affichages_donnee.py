1b0ee367f6af11db65e95796bc9738ab6d171a66
Sjats <149076885+Sjats@users.noreply.github.com>
2024-05-06 09:57:28 +0200

    test_affichage modif

--- tests/test_affichages_donnee.py
+++ tests/test_affichages_donnee.py
@@ -4,10 +4,10 @@ from classes.affichages_donnee import AffichageDonnees
 
 
 @pytest.mark.parametrize(
-    'affichage', 'erreur', 'message_erreur',
+    'indices_produits', "indices_categories", 'erreur', 'message_erreur',
     [
         ([['France', 0.65, 1.0], ['Italy', 0.83, 1.28]],
-        {'France': [0.65, 1.0], 'Italy': [0.83, 1.28]},
+         {'France': [0.65, 1.0], 'Italy': [0.83, 1.28]},
          TypeError, (
              "les indices des produits doivent"
              " être renseignés sous la forme "
@@ -15,7 +15,7 @@ from classes.affichages_donnee import AffichageDonnees
          )),
 
         ({'France': [0.65, 1.0], 'Italy': [0.83, 1.28]},
-                          [['France', 0.65, 1.0], ['Italy', 0.83, 1.28]],
+         [['France', 0.65, 1.0], ['Italy', 0.83, 1.28]],
          TypeError, (
              "Les indices des catégories de produits"
              " doivent être renseignés sous "
@@ -23,6 +23,35 @@ from classes.affichages_donnee import AffichageDonnees
          ))
      ]
 )
-def test_produit_init_echec(affichage, erreur, message_erreur):
+def test_produit_init_echec(indices_produits,
+                            indices_categories,
+                            erreur,
+                            message_erreur):
+
     with pytest.raises(erreur, match=re.escape(message_erreur)):
-        AffichageDonnees(affichage)
+        AffichageDonnees(indices_produits, indices_categories,)
+
+
+@pytest.mark.parametrize(
+    'indices_produits', "indices_categories",
+    [
+        ({['France', 0.65, 1.0], ['Italy', 0.83, 1.28]},
+         {'France': [0.65, 1.0], 'Italy': [0.83, 1.28]},
+         TypeError, (
+             "les indices des produits doivent"
+             " être renseignés sous la forme "
+             "d'un dictionnaire."
+         )),
+
+        ({'France': [0.65, 1.0], 'Italy': [0.83, 1.28]},
+         {['France', 0.65, 1.0], ['Italy', 0.83, 1.28]},
+         TypeError, (
+             "Les indices des catégories de produits"
+             " doivent être renseignés sous "
+             "la forme d'un dictionnaire."
+         ))
+     ]
+)
+def test_produit_init_succes(indices_produits, indices_categories,):
+
+    AffichageDonnees(indices_produits, indices_categories,)
