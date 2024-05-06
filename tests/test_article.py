80a3a7f4405f132fa44d1b68cd840501f83ce16d
Sjats <149076885+Sjats@users.noreply.github.com>
2024-05-06 10:56:30 +0200

    test article 100 %

--- tests/test_article.py
+++ tests/test_article.py
@@ -5,20 +5,38 @@ from classes.prix import Prix
 
 
 @pytest.mark.parametrize(
-    'article', 'erreur', 'message_erreur',
+    'id_article, prix, pays, erreur, message_erreur',
     [
-        (Article({'Pays': 'France', 'Nom': 'Coffee+maker', 'Numéro': 0},
-                 Prix(30, 'EUR'),
-                 'France'),
+        ({'Pays': 'France', 'Nom': 'Coffee+maker', 'Numéro': 0},
+         Prix("EUR", 30),
+         'France',
          TypeError, "id_article doit être de type str"),
 
-        (Article('France/Coffee+maker/0', 30, 'France'),
+        ('France/Coffee+maker/0',
+         30,
+         'France',
          TypeError, "prix doit être de type Prix"),
 
-        (Article('France/Coffee+maker/0', Prix('EUR', 30), ['France']),
+        ('France/Coffee+maker/0',
+         Prix('EUR', 30),
+         ['France'],
          TypeError, "pays doit être de type str"),
      ]
 )
-def test_produit_init_echec(article, erreur, message_erreur):
+def test_produit_init_echec(id_article, prix, pays, erreur, message_erreur):
     with pytest.raises(erreur, match=re.escape(message_erreur)):
-        article
+        Article(id_article, prix, pays)
+
+
+@pytest.mark.parametrize(
+    'id_article, prix, pays',
+    [
+        ('France/Coffee+maker/0',
+         Prix("EUR", 30),
+         'France')
+
+     ]
+)
+def test_produit_init_succes(id_article, prix, pays):
+    art = Article(id_article, prix, pays)
+    str(art)
